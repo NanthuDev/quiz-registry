be99f092186aa15b19f762977f19b060840e8108
Nantha <nanthu1593@gmail.com>
2024-08-15 00:22:56 +0800

    enable eureka

--- src/main/java/com/nkspring/quiz_registry/QuizRegistryApplication.java
+++ src/main/java/com/nkspring/quiz_registry/QuizRegistryApplication.java
@@ -2,8 +2,10 @@ package com.nkspring.quiz_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class QuizRegistryApplication {
 
 	public static void main(String[] args) {
