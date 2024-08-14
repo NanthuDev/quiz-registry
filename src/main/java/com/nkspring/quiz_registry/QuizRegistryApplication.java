9401a974f67c8bd53a8681da15e0bd0dfe6ba7ad
Nantha <nanthu1593@gmail.com>
2024-08-14 23:55:37 +0800

    Registry source

---
+++ src/main/java/com/nkspring/quiz_registry/QuizRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.nkspring.quiz_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizRegistryApplication.class, args);
+	}
+
+}
